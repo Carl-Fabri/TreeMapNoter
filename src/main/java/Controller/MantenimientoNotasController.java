ff255ae55abd9b17ae604b58d80fc777fcdd5558
Carl-Fabri <ecefabriciof@gmail.com>
2024-06-26 01:00:44 -0500

    uploadProyect

---
+++ src/main/java/Controller/MantenimientoNotasController.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import Model.Note;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Carlos Fabricio
+ */
+public class MantenimientoNotasController {
+    
+    private TreeMap<LocalDateTime, Note> noteMap;
+
+    public MantenimientoNotasController() {
+        noteMap = new TreeMap<>();
+    }
+    
+    public void InsertProducts(){
+        noteMap.put(LocalDateTime.of(2023, 1, 1, 12, 0), new Note("Dia 1", "El dia comenzo caido como el caudal de una catarata.", LocalDateTime.of(2023, 1, 1, 12, 0), LocalDateTime.of(2023, 1, 1, 12, 0)));
+        noteMap.put(LocalDateTime.of(2022, 1, 2, 11, 50), new Note("Dia 2", "Hay golpes en la vida, tan fuertes yo no se, golpes como el odio de...", LocalDateTime.of(2022, 1, 2, 11, 50), LocalDateTime.of(2022, 1, 2, 11, 50)));
+        noteMap.put(LocalDateTime.of(2021, 6, 2, 9, 50), new Note("Dia 3", "De pronto nace toda aquella esperanza, nace tu sonrisa.", LocalDateTime.of(2021, 6, 2, 9, 50), LocalDateTime.of(2021, 6, 2, 9, 50)));
+        noteMap.put(LocalDateTime.of(2020, 3, 19,2,20), new Note("Dia 4", "Anelado recuerdo de persona nostalgico, aparece ante mi y dame la esperanza....", LocalDateTime.of(2020, 3, 19,2,20), LocalDateTime.of(2020, 3, 19,2,20)));
+    }
+
+    public void addBook(Note note) {
+        noteMap.put(note.getCreateAt(), note);
+    }
+    
+    public void removeNote(LocalDateTime key) {
+        noteMap.remove(key);
+    }
+
+    public TreeMap<LocalDateTime, Note> getBookMap() {
+        return noteMap;
+    }
+}
---
+++ src/main/java/Model/Note.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Carlos Fabricio
+ */
+public class Note {
+
+    public Note(String name, String message, LocalDateTime updateAt, LocalDateTime createAt) {
+        this.name = name;
+        this.message = message;
+        this.updateAt = updateAt;
+        this.createAt = createAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    public LocalDateTime getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(LocalDateTime createAt) {
+        this.createAt = createAt;
+    }
+    
+    String name;
+    String message;
+    LocalDateTime updateAt;
+    LocalDateTime createAt;
+}
